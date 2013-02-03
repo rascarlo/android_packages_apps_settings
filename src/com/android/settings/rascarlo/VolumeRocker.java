22a3e461f97898216efa039e8f73c88f6e634ab3
DvTonder <david.vantonder@gmail.com>
2013-02-03 10:30:09 -0500

    Audio: make safe headset volume user configurable

--- src/com/android/settings/rascarlo/VolumeRocker.java
+++ src/com/android/settings/rascarlo/VolumeRocker.java
@@ -15,11 +15,13 @@ public class VolumeRocker extends SettingsPreferenceFragment implements OnPrefer
 
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
     private static final String KEY_VOLBTN_MUSIC_CTRL = "volbtn_music_controls";
-    private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+    private static final String KEY_VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+    private static final String KEY_SAFE_HEADSET_VOLUME = "safe_headset_volume";
 
     private CheckBoxPreference mVolumeWake;
     private CheckBoxPreference mVolBtnMusicCtrl;
     private ListPreference mVolumeKeyCursorControl;
+    private CheckBoxPreference mSafeHeadsetVolume;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,21 +29,32 @@ public class VolumeRocker extends SettingsPreferenceFragment implements OnPrefer
 
         addPreferencesFromResource(R.xml.volume_rocker_settings);
 
+        // volume rocker wake
         mVolumeWake = (CheckBoxPreference) findPreference(KEY_VOLUME_WAKE);
         mVolumeWake.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                     Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
 
+        // volume rocker music control
         mVolBtnMusicCtrl = (CheckBoxPreference) findPreference(KEY_VOLBTN_MUSIC_CTRL);
         mVolBtnMusicCtrl.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                    Settings.System.VOLBTN_MUSIC_CONTROLS, 0) == 1);
 
-        mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
+        // volume rocker cursor control
+        mVolumeKeyCursorControl = (ListPreference) findPreference(KEY_VOLUME_KEY_CURSOR_CONTROL);
         if(mVolumeKeyCursorControl != null) {
             mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
             mVolumeKeyCursorControl.setValue(Integer.toString(Settings.System.getInt(getActivity()
                     .getContentResolver(), Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0)));
             mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
         }
+
+        // volume safe head set
+        mSafeHeadsetVolume = (CheckBoxPreference) findPreference(KEY_SAFE_HEADSET_VOLUME);
+        mSafeHeadsetVolume.setPersistent(false);
+        boolean safeMediaVolumeEnabled = getResources().getBoolean(
+                com.android.internal.R.bool.config_safe_media_volume_enabled);
+        mSafeHeadsetVolume.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.SAFE_HEADSET_VOLUME, safeMediaVolumeEnabled ? 1 : 0) != 0);
     }
 
     @Override
@@ -56,6 +69,11 @@ public class VolumeRocker extends SettingsPreferenceFragment implements OnPrefer
                     Settings.System.VOLBTN_MUSIC_CONTROLS,
                     mVolBtnMusicCtrl.isChecked()
                     ? 1 : 0);
+         } else if (preference == mSafeHeadsetVolume) {
+             Settings.System.putInt(getActivity().getContentResolver(),
+                     Settings.System.SAFE_HEADSET_VOLUME,
+                     mSafeHeadsetVolume.isChecked()
+                     ? 1 : 0);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
