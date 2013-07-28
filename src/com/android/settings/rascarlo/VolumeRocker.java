8a446a1a34912b878143584512da4479b3d42897
rascarlo <rascarlo@gmail.com>
2013-07-28 17:10:29 +0200

    volume rocker: volume adjust sound preference

---
+++ src/com/android/settings/rascarlo/VolumeRocker.java
@@ -0,0 +1,41 @@
+
+package com.android.settings.rascarlo;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class VolumeRocker extends SettingsPreferenceFragment {
+
+    private static final String KEY_VOLUME_ADJUST_SOUNDS = "volume_adjust_sounds";
+
+    private CheckBoxPreference mVolumeAdjustSounds;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.volume_rocker_settings);
+
+        mVolumeAdjustSounds = (CheckBoxPreference) findPreference(KEY_VOLUME_ADJUST_SOUNDS);
+        mVolumeAdjustSounds.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED, 1) != 0);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mVolumeAdjustSounds) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED,
+                    mVolumeAdjustSounds.isChecked() ? 1 : 0);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+}
