d0bd57fedd8228b53b5b875b3f171deb919faf20
rascarlo <rascarlo@gmail.com>
2015-10-20 10:17:46 +0200

    always return true for showEnableMultiWindowPreference

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -817,7 +817,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     }
 
     private static boolean showEnableMultiWindowPreference() {
-        return !"user".equals(Build.TYPE);
+//        return !"user".equals(Build.TYPE);
+        return true;
     }
 
     private void setEnableMultiWindow(boolean value) {
